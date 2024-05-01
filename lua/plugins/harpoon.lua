cc8d81e5cc47b19b6af77768a1bc8e91c7ef133d
Axel <axel.deb.len@gmail.com>
2024-05-01 10:41:55 +1000

    more harpoons

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,12 +6,18 @@ return {
 
     harpoon:setup()
 
-    vim.keymap.set('n', '<leader>h', function() harpoon:list():append() end)
+    vim.keymap.set('n', '<leader>h', function() harpoon:list():add() end)
     vim.keymap.set('n', '<C-a>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
     vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
     vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
     vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
+    vim.keymap.set('n', '<leader>5', function() harpoon:list():select(1) end)
+    vim.keymap.set('n', '<leader>6', function() harpoon:list():select(2) end)
+    vim.keymap.set('n', '<leader>7', function() harpoon:list():select(3) end)
+    vim.keymap.set('n', '<leader>8', function() harpoon:list():select(4) end)
+    vim.keymap.set('n', '<leader>9', function() harpoon:list():select(4) end)
+
 
     harpoon:extend({
       UI_CREATE = function(cx)
