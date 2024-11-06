14acbb459e19f4ff1d2702f09d31ca04c98d942e
Axel <axel.deb.len@gmail.com>
2024-11-06 12:04:29 +1100

    back to this

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,16 +2,15 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    }
+  },
 
-  config = function ()
-    require('oil').setup({
-      default_file_explorer = true,
-      view_options = {
-        show_hidden = true,
-      }
-    }), 
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = {
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  end
+  }
 }
