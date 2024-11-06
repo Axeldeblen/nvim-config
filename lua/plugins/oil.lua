881bb19858e0f714e243fc0129126a44d8203ff5
Axel <axel.deb.len@gmail.com>
2024-11-06 12:02:50 +1100

    this syntax

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,17 @@
 return {
   'stevearc/oil.nvim',
-  opts = {
-    default_file_explorer = true,
-    view_options = {
-      show_hidden = true,
-    }
-  },
-
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = {
+
+  config = function ()
+    require('oil').setup({
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      }
+    }), 
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  }
+  end
 }
