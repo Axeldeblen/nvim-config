3ebe586e3907f8a6f10d7f1b77899536398c94b2
Axel <axel.deb.len@gmail.com>
2025-06-30 10:28:53 +1000

    fix noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,4 +1,4 @@
-{
+return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
@@ -11,5 +11,5 @@
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-    }
+  }
 }
